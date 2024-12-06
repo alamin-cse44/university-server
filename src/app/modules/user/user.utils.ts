4fc87d8ed1a99cbf9cb8f27e24581320d4d2d9db
alamin-cse44 <alamincse44@gmail.com>
2024-12-06 16:29:01 +0600

    Fix: generated id sem wise

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -16,14 +16,30 @@ const findLastStudentId = async () => {
     })
     .lean();
   // 2030 01 0001
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
   // first time 0000
   // 0001 => 1
 
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId();
+
+  // 2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
 
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
