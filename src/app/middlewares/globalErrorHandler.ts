de677f330cf852eb7ad8a0f0cf42ef30e0561f70
alamin-cse44 <alamincse44@gmail.com>
2024-12-08 16:58:31 +0600

    handle organized zod err

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
 
 const globalErrorHandler = (
   error: any,
@@ -6,13 +9,44 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || 'Something went wrong!';
+  let statusCode = error.statusCode || 500;
+  let message = error.message || 'Something went wrong!';
+
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const statusCode = 400;
+    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+
+    return {
+      statusCode,
+      message: 'Validation error',
+      errorSources,
+    };
+  };
+
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: error,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? error.stack : null,
   });
 };
 
