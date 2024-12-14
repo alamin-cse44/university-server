8c94373e9a5c796900a88ae3b5895d5e67b0c60e
alamin-cse44 <alamincse44@gmail.com>
2024-12-14 21:21:15 +0600

    Add: semester reg interface, model, validation

---
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -0,0 +1,44 @@
+import { model, Schema } from 'mongoose';
+import { TSemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistrationStatus } from './semesterRegistration.constant';
+
+const semesterRegistrationSchema = new Schema<TSemesterRegistration>(
+  {
+    acamemicSemester: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      required: true,
+      unique: true,
+    },
+    status: {
+      type: String,
+      enum: SemesterRegistrationStatus,
+      required: true,
+      default: 'UPCOMING',
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    minCredit: {
+      type: Number,
+      default: 3,
+    },
+    maxCredit: {
+      type: Number,
+      default: 15,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const SemesterRegistration = model<TSemesterRegistration>(
+  'SemesterRegistration',
+  semesterRegistrationSchema,
+);
