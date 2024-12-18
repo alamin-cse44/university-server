22c7757509cf2d9e4ecdf80ca2ba488522d60c01
alamin-cse44 <alamincse44@gmail.com>
2024-12-18 13:11:47 +0600

    Add: authorization

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -4,8 +4,9 @@ import AppError from '../errors/AppError';
 import { StatusCodes } from 'http-status-codes';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
+import { TUserRole } from '../modules/user/user.interface';
 
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
 
@@ -19,9 +20,16 @@ const auth = () => {
       if(err){
         throw new AppError(StatusCodes.UNAUTHORIZED, 'Invalid token! You are not authorized!!!');
       }
+
+      const role = (decoded as JwtPayload).userRole;
+      console.log("role: ", role)
+      console.log("required role: ", requiredRoles)
+      if(requiredRoles && !requiredRoles.includes(role)){
+        throw new AppError(StatusCodes.FORBIDDEN, 'You do not have the necessary permissions to access this resource!');
+      }
+
       // decoded undefined
       req.user = decoded as JwtPayload;
-      console.log(req.user.data)
     });
 
     next();
--- src/app/modules/user/user.router.ts
+++ src/app/modules/user/user.router.ts
@@ -4,11 +4,14 @@ import { studentValidations } from '../student/student.validation';
 import validateRequest from '../../middlewares/validateRequest';
 import { facultyValidations } from '../Faculty/faculty.validaiton';
 import { AdminValidations } from '../admin/admin.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
 router.post(
   '/create-student',
+  auth(USER_ROLE.admin),
   validateRequest(studentValidations.createStudentValidationSchema),
   UserControllers.createStudent,
 );
